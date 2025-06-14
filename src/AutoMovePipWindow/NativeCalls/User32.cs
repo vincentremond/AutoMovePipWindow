216be2dac2e08bc2490560af92262e31157b669e
Vincent Rémond <vincent.remond@gmail.com>
2025-06-14 15:53:50 +0200

    quickstart

--- src/AutoMovePipWindow/NativeCalls/User32.cs
+++ src/AutoMovePipWindow/NativeCalls/User32.cs
@@ -1,31 +1,30 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-namespace AutoMovePipWindow.NativeCalls
+namespace AutoMovePipWindow.NativeCalls;
+
+internal static class User32
 {
-    internal static class User32
-    {
-        internal const int GwlExStyle = -20;
-        internal const int WsExAppwindow = 0x00040000;
-        internal const int WsExToolwindow = 0x00000080;
+    internal const int GwlExStyle = -20;
+    internal const int WsExAppwindow = 0x00040000;
+    internal const int WsExToolwindow = 0x00000080;
 
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className, string windowTitle);
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className, string windowTitle);
 
-        [DllImport("user32.dll", SetLastError = true)]
-        internal static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+    [DllImport("user32.dll", SetLastError = true)]
+    internal static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
-        [DllImport("User32.dll")]
-        public static extern bool MoveWindow(IntPtr handle, int x, int y, int width, int height, bool redraw);
+    [DllImport("User32.dll")]
+    public static extern bool MoveWindow(IntPtr handle, int x, int y, int width, int height, bool redraw);
 
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool GetWindowRect(IntPtr hWnd, out Rectangle lpRect);
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool GetWindowRect(IntPtr hWnd, out Rectangle lpRect);
 
-        [DllImport("user32.dll", SetLastError = true)]
-        internal static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+    [DllImport("user32.dll", SetLastError = true)]
+    internal static extern int GetWindowLong(IntPtr hWnd, int nIndex);
 
-        [DllImport("user32.dll")]
-        internal static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-    }
+    [DllImport("user32.dll")]
+    internal static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
 }
--- src/AutoMovePipWindow/Program.cs
+++ src/AutoMovePipWindow/Program.cs
@@ -10,53 +10,52 @@ using AutoMovePipWindow.Contracts;
 using AutoMovePipWindow.Services;
 using Microsoft.Extensions.Configuration;
 
-namespace AutoMovePipWindow
+namespace AutoMovePipWindow;
+
+internal static class Program
 {
-    internal static class Program
+    [STAThread]
+    public static async Task Main()
     {
-        [STAThread]
-        public static async Task Main()
-        {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+        Application.SetHighDpiMode(HighDpiMode.SystemAware);
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
 
-            var cancellationTokenSource = new CancellationTokenSource();
-            var cancellationToken = cancellationTokenSource.Token;
+        var cancellationTokenSource = new CancellationTokenSource();
+        var cancellationToken = cancellationTokenSource.Token;
 
-            var t = Worker(cancellationToken);
+        var t = Worker(cancellationToken);
 
-            Application.Run(new TrayIconContext(cancellationTokenSource));
+        Application.Run(new TrayIconContext(cancellationTokenSource));
 
-            await t;
-        }
+        await t;
+    }
 
-        private static async Task Worker(CancellationToken cancellationToken)
-        {
-            var serviceProvider = InitApp();
-            var daemon = serviceProvider.GetRequiredService<IServiceDaemon>();
-            await daemon.StartAsync(cancellationToken);
-        }
+    private static async Task Worker(CancellationToken cancellationToken)
+    {
+        var serviceProvider = InitApp();
+        var daemon = serviceProvider.GetRequiredService<IServiceDaemon>();
+        await daemon.StartAsync(cancellationToken);
+    }
 
-        private static IServiceProvider InitApp()
-        {
-            var config = GetConfiguration<PipConfiguration>();
-            var serviceProvider = new ServiceCollection();
-            serviceProvider.AddSingleton(config);
-            serviceProvider.AddTransient<ISingleInstanceChecker, SingleInstanceChecker>();
-            serviceProvider.AddTransient<ScreenConfigurationLocator, ScreenConfigurationLocator>();
-            serviceProvider.AddTransient<IServiceDaemon, ServiceDaemon>();
-            serviceProvider.AddLogging(builder => builder.AddConsole(options => options.LogToStandardErrorThreshold = LogLevel.Trace));
-            serviceProvider.AddSingleton(config);
+    private static IServiceProvider InitApp()
+    {
+        var config = GetConfiguration<PipConfiguration>();
+        var serviceProvider = new ServiceCollection();
+        serviceProvider.AddSingleton(config);
+        serviceProvider.AddTransient<ISingleInstanceChecker, SingleInstanceChecker>();
+        serviceProvider.AddTransient<ScreenConfigurationLocator, ScreenConfigurationLocator>();
+        serviceProvider.AddTransient<IServiceDaemon, ServiceDaemon>();
+        serviceProvider.AddLogging(builder => builder.AddConsole(options => options.LogToStandardErrorThreshold = LogLevel.Trace));
+        serviceProvider.AddSingleton(config);
 
-            return serviceProvider.BuildServiceProvider();
-        }
+        return serviceProvider.BuildServiceProvider();
+    }
 
-        private static T GetConfiguration<T>()
-        {
-            var content = File.ReadAllText("appsettings.yml");
-            var deserializer = new YamlDotNet.Serialization.Deserializer();
-            return deserializer.Deserialize<T>(content);
-        }
+    private static T GetConfiguration<T>()
+    {
+        var content = File.ReadAllText("appsettings.yml");
+        var deserializer = new YamlDotNet.Serialization.Deserializer();
+        return deserializer.Deserialize<T>(content);
     }
 }
