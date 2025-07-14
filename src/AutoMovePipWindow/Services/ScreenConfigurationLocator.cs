b5eae03f2e540f702175901bc85a7412a43e6b2d
Vincent Rémond <vincent.remond@gmail.com>
2025-07-14 10:30:31 +0200

    quickstart

--- src/AutoMovePipWindow/Services/ScreenConfigurationLocator.cs
+++ src/AutoMovePipWindow/Services/ScreenConfigurationLocator.cs
@@ -7,114 +7,113 @@ using AutoMovePipWindow.Configuration;
 using AutoMovePipWindow.DTO;
 using Z.Expressions;
 
-namespace AutoMovePipWindow.Services
+namespace AutoMovePipWindow.Services;
+
+internal class ScreenConfigurationLocator
 {
-    internal class ScreenConfigurationLocator
+    private readonly PipConfiguration _configuration;
+
+    public ScreenConfigurationLocator(PipConfiguration configuration)
     {
-        private readonly PipConfiguration _configuration;
+        _configuration = configuration;
+    }
 
-        public ScreenConfigurationLocator(PipConfiguration configuration)
+    internal List<PositionTargetAssociation> GetTargets(Screen[] allScreens)
+    {
+        var assoc = AssociateScreenAndPositions(allScreens);
+        if (assoc == null)
         {
-            _configuration = configuration;
-        }
-
-        internal List<PositionTargetAssociation> GetTargets(Screen[] allScreens)
-        {
-            var assoc = AssociateScreenAndPositions(allScreens);
-            if (assoc == null)
-            {
-                return null;
-            }
-
-            return PositionTargetAssociations(assoc);
-        }
-
-        private List<PositionTargetAssociation> PositionTargetAssociations(ScreenPositions assoc)
-        {
-            var results = new List<PositionTargetAssociation>();
-            foreach (var position in assoc.Positions)
-            {
-                var cursorScreenIndex = position.Cursor.Screen;
-                var rectangle = GetRectangle(assoc.Screens[cursorScreenIndex].Bounds, position.Cursor);
-                var targetScreen = assoc.Screens[position.Target.Screen];
-                var targetPosition = position.Target.Position;
-                var size = position.Target.Size ?? _configuration.Size;
-                results.Add(new PositionTargetAssociation(rectangle, targetScreen, targetPosition, size));
-            }
-
-            return results;
-        }
-
-        private Rectangle GetRectangle(Rectangle screenBounds, CursorConfiguration positionCursor)
-        {
-            var x = (int) (screenBounds.X + (positionCursor.X * screenBounds.Width));
-            var y = (int) (screenBounds.Y + (positionCursor.Y * screenBounds.Height));
-            var width = (int) (positionCursor.Width * screenBounds.Width);
-            var height = (int) (positionCursor.Height * screenBounds.Height);
-            return new Rectangle(x, y, width, height);
-        }
-
-        private ScreenPositions AssociateScreenAndPositions(Screen[] allScreens)
-        {
-            foreach (var (_, screenConfiguration) in _configuration.Screens)
-            {
-                if (screenConfiguration.Conditions.Length != allScreens.Length)
-                {
-                    continue;
-                }
-
-                var matchedScreens = GetOrderedScreens(allScreens, screenConfiguration.Conditions);
-                if (matchedScreens == null)
-                {
-                    continue;
-                }
-
-                return new ScreenPositions(matchedScreens, screenConfiguration.Positions);
-            }
-
             return null;
         }
 
-        private Screen[] GetOrderedScreens(Screen[] allScreens, ConditionsConfiguration[] conditions)
+        return PositionTargetAssociations(assoc);
+    }
+
+    private List<PositionTargetAssociation> PositionTargetAssociations(ScreenPositions assoc)
+    {
+        var results = new List<PositionTargetAssociation>();
+        foreach (var position in assoc.Positions)
         {
-            var screens = allScreens.ToList();
-            var result = new List<Screen>();
-            foreach (var condition in conditions)
-            {
-                var screen = screens.FirstOrDefault(s => EvaluateScreenAndConditions(s, condition));
-
-                if (screen != null)
-                {
-                    screens.Remove(screen);
-                    result.Add(screen);
-                }
-                else
-                {
-                    return null;
-                }
-            }
-
-            if (screens.Count > 0)
-            {
-                throw new ApplicationException("should not happen");
-            }
-
-            return result.ToArray();
+            var cursorScreenIndex = position.Cursor.Screen;
+            var rectangle = GetRectangle(assoc.Screens[cursorScreenIndex].Bounds, position.Cursor);
+            var targetScreen = assoc.Screens[position.Target.Screen];
+            var targetPosition = position.Target.Position;
+            var size = position.Target.Size ?? _configuration.Size;
+            results.Add(new PositionTargetAssociation(rectangle, targetScreen, targetPosition, size));
         }
 
-        private bool EvaluateScreenAndConditions(Screen screen, ConditionsConfiguration condition)
+        return results;
+    }
+
+    private Rectangle GetRectangle(Rectangle screenBounds, CursorConfiguration positionCursor)
+    {
+        var x = (int) (screenBounds.X + (positionCursor.X * screenBounds.Width));
+        var y = (int) (screenBounds.Y + (positionCursor.Y * screenBounds.Height));
+        var width = (int) (positionCursor.Width * screenBounds.Width);
+        var height = (int) (positionCursor.Height * screenBounds.Height);
+        return new Rectangle(x, y, width, height);
+    }
+
+    private ScreenPositions AssociateScreenAndPositions(Screen[] allScreens)
+    {
+        foreach (var (_, screenConfiguration) in _configuration.Screens)
         {
-            if (condition.Primary.HasValue && condition.Primary.Value != screen.Primary)
+            if (screenConfiguration.Conditions.Length != allScreens.Length)
             {
-                return false;
+                continue;
             }
 
-            if (condition.Expression != null && !Eval.Execute<bool>(condition.Expression, screen.Bounds))
+            var matchedScreens = GetOrderedScreens(allScreens, screenConfiguration.Conditions);
+            if (matchedScreens == null)
             {
-                return false;
+                continue;
             }
 
-            return true;
+            return new ScreenPositions(matchedScreens, screenConfiguration.Positions);
         }
+
+        return null;
+    }
+
+    private Screen[] GetOrderedScreens(Screen[] allScreens, ConditionsConfiguration[] conditions)
+    {
+        var screens = allScreens.ToList();
+        var result = new List<Screen>();
+        foreach (var condition in conditions)
+        {
+            var screen = screens.FirstOrDefault(s => EvaluateScreenAndConditions(s, condition));
+
+            if (screen != null)
+            {
+                screens.Remove(screen);
+                result.Add(screen);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        if (screens.Count > 0)
+        {
+            throw new ApplicationException("should not happen");
+        }
+
+        return result.ToArray();
+    }
+
+    private bool EvaluateScreenAndConditions(Screen screen, ConditionsConfiguration condition)
+    {
+        if (condition.Primary.HasValue && condition.Primary.Value != screen.Primary)
+        {
+            return false;
+        }
+
+        if (condition.Expression != null && !Eval.Execute<bool>(condition.Expression, screen.Bounds))
+        {
+            return false;
+        }
+
+        return true;
     }
 }
--- src/AutoMovePipWindow/Services/SingleInstanceChecker.cs
+++ src/AutoMovePipWindow/Services/SingleInstanceChecker.cs
@@ -2,24 +2,23 @@
 using System.Linq;
 using AutoMovePipWindow.Contracts;
 
-namespace AutoMovePipWindow.Services
-{
-    internal class SingleInstanceChecker : ISingleInstanceChecker
-    {
-        public void KillOtherInstances()
-        {
-            var currentProcess = Process.GetCurrentProcess();
-            if (currentProcess.ProcessName.StartsWith("dotnet"))
-            {
-                return;
-            }
+namespace AutoMovePipWindow.Services;
 
-            var allOtherProcesses = Process.GetProcessesByName(currentProcess.ProcessName)
-                .Where(p => p.Id != currentProcess.Id);
-            foreach (var p in allOtherProcesses)
-            {
-                p.Kill();
-            }
+internal class SingleInstanceChecker : ISingleInstanceChecker
+{
+    public void KillOtherInstances()
+    {
+        var currentProcess = Process.GetCurrentProcess();
+        if (currentProcess.ProcessName.StartsWith("dotnet"))
+        {
+            return;
+        }
+
+        var allOtherProcesses = Process.GetProcessesByName(currentProcess.ProcessName)
+            .Where(p => p.Id != currentProcess.Id);
+        foreach (var p in allOtherProcesses)
+        {
+            p.Kill();
         }
     }
 }
